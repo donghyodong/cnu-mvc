afd5bae6a4ccd3466fc0b34bcf437887188dafd8
donghyodong <156331168+donghyodong@users.noreply.github.com>
2025-04-21 15:44:46 +0900

    Update MemberService.java

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -10,12 +10,21 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member join(Member member){
+        if(memberRepository.findByEmail(member.getEmail()) != null) {
+            throw new IllegalStateException("이미 존재하는 이메일입니다.");
+        }
+
         return memberRepository.save(member);
     }
 
     public Member validateMember(String email, String pwd) {
-        Member findMember = findById(1L);
-        return findMember;
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null || !member.getPwd().equals(pwd)) {
+            throw new IllegalArgumentException("이메일 또는 비밀번호를 확인해주세요.");
+        }
+
+        return member;
     }
 
     public Member findById(Long id) {
